010aade3b5e5ce005a72c13a31dd454d5cfd32f3
Nelfimov <nelfimov@mail.ru>
2022-12-13 20:29:50 +0300

    added user schema and model

--- app.js
+++ app.js
@@ -22,8 +22,7 @@ const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error: '));
 
 // view engine setup
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = dirname(fileURLToPath(import.meta.url));
 app.set('views', join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
