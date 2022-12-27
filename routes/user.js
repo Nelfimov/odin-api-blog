5829f5ebe83716018654b52e9810fc1d9ad3b2f1
nelfimov <nelfimov@mail.ru>
2022-12-27 10:49:20 +0300

    added login and protected route

--- routes/user.js
+++ routes/user.js
@@ -3,30 +3,80 @@ import passport from '../config/passport.js';
 import bcrypt from 'bcryptjs';
 import User from '../models/user.js';
 import {issueJWT} from '../config/jwt.js';
+import async from 'async';
 
 const customRouter = router();
 
-customRouter.post('/login', passport.authenticate('jwt', {session: false}));
+customRouter.get(
+    '/protected',
+    passport.authenticate('jwt', {session: false}),
+    (req, res, next) => {
+      res.status(200).json({message: 'You are authorized'});
+    },
+);
 
-customRouter.post('/register', (req, res, next) => {
-  const {password} = req.body;
-  bcrypt.hash(password, 10, async (err, hashedPassword) => {
+customRouter.post('/login', (req, res, next) => {
+  const {username, password} = req.body;
+
+  User.findOne({username}, (err, user) => {
     if (err) return next(err);
 
-    const {username, email} = req.body;
+    if (!user) {
+      return res.json({message: 'No such user found'});
+    };
 
-    const user = new User({
-      username, email, password: hashedPassword,
+    bcrypt.compare(password, user.password, (err, success) => {
+      if (err) return next(err);
+
+      if (success) {
+        const jwt = issueJWT(user);
+
+        return res.json({
+          message: 'Success, you are logged in',
+          token: jwt.token,
+          user,
+          expiresIn: jwt.expires,
+        });
+      };
+
+      return res.json({message: 'Password is incorrect'});
     });
+  });
+});
 
-    await user.save();
-    const jwt = issueJWT(user);
+customRouter.post('/register', (req, res, next) => {
+  const {password, username, email} = req.body;
 
-    return res.json({
-      message: 'Success, new user registered. You can now log in',
-      user: user,
-      token: jwt.token,
-      expiresIn: jwt.expires,
+  async.parallel({
+    username(callback) {
+      User.find({username: username}).exec(callback);
+    },
+    email(callback) {
+      User.find({email: email}).exec(callback);
+    },
+  }, (err, result) => {
+    if (err) return next(err);
+
+    if (!!result.username.length || !!result.email.length) {
+      return res.json({message: 'This username or email is already taken'});
+    };
+
+    bcrypt.hash(password, 10, async (err, hashedPassword) => {
+      if (err) return next(err);
+
+      const user = new User({
+        username, email, password: hashedPassword,
+      });
+
+      await user.save();
+      const jwt = issueJWT(user);
+
+      return res.json({
+        message: 'Success, new user registered. You can now log in',
+        user: user,
+        token: jwt.token,
+        expiresIn: jwt.expires,
+      });
     });
   });
 });
